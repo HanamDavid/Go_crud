2d59a0a309d207cfec8112447f233c4abeb03dbc
HanamDavid <GiwmHanam@protonmail.com>
2024-04-23 12:52:54 -0500

    making sure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HanamDavid/Go_crud.git
+
+go 1.22.2
---
+++ tasks/task.go
@@ -0,0 +1,8 @@
+
+package task
+
+type Task struct{
+	ID int `json:id`
+	Name string `json:"name"`
+	Complete bool `json:"complete"`
+}
